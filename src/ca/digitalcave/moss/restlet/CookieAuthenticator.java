0a3eab80051bc6fa98aafd3d46b80fd5a15930c4
Warren Janssens <janssensw@richer.ca>
2013-11-16 11:58:27 -0700

    remove sysout

--- src/ca/digitalcave/moss/restlet/CookieAuthenticator.java
+++ src/ca/digitalcave/moss/restlet/CookieAuthenticator.java
@@ -229,7 +229,6 @@ public class CookieAuthenticator extends ChallengeAuthenticator {
 		final String authenticator = cr.getParameters().getFirstValue("authenticator");
 		if (authenticator != null) p.set("authenticator", authenticator);
 
-		System.out.println(p.getQueryString());
 		return p.getQueryString();
 	}
 
