80ae90e8e1a50c0c548147cfa8791e6da2317602
Wyatt <wyatt@digitalcave.ca>
2014-01-22 12:11:06 -0700

    Change default animation timeout to 10 seconds (rather than 3)

--- src/ca/digitalcave/moss/restlet/login/resource/view/AnimatedLabel.js
+++ src/ca/digitalcave/moss/restlet/login/resource/view/AnimatedLabel.js
@@ -4,7 +4,7 @@ Ext.define("Login.view.AnimatedLabel", {
 	"text": "\xA0",	//non breaking space to reserve space for text and not collapse empty label
 	
 	"setTextAnimated": function(text, duration){
-		if (duration == null) duration = 3000;
+		if (duration == null) duration = 10000;
 		this.stopAnimation();
 		this.animate({"to": {"opacity": 0},"duration": 0});	//Set label opacity to 0 initially; this only matters on first call for a given label.  TODO How do we set opacity without animation?
 		this.setText(text);
